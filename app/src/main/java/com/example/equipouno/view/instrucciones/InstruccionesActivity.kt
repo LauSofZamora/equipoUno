482a8ae5802091c557c477eb435afcac26553d23
juan <jjla1326@gmail.com>
2024-10-26 16:43:14 -0500

    boton de atras en instrucciones funcionando

--- app/src/main/java/com/example/equipouno/view/instrucciones/InstruccionesActivity.kt
+++ app/src/main/java/com/example/equipouno/view/instrucciones/InstruccionesActivity.kt
@@ -1,13 +1,27 @@
 package com.example.equipouno.view.instrucciones
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import com.example.equipouno.R
+import com.example.equipouno.databinding.ActivityInstruccionesBinding
 
 class InstruccionesActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityInstruccionesBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_instrucciones) // Establece el layout gris oscuro
+        binding = ActivityInstruccionesBinding.inflate(layoutInflater)
+        setContentView(binding.root) // Usa binding.root para establecer el layout
+
+        setSupportActionBar(binding.toolbar3)
+        supportActionBar?.title = "" // Elimina el título de la Toolbar
+
+        // Define la acción al presionar el botón "Atrás"
+        binding.btnBack.setOnClickListener {
+            onBackPressed() // Llama a onBackPressed() para regresar a la actividad anterior
+        }
     }
 }
