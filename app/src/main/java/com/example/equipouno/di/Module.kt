f362ec2e7f113b49a968b3de126024b1cebaf9e0
Laura Sofia Zamora <zamora.laura@correounivalle.edu.co>
2024-12-04 00:25:44 -0500

    Aplicación de dagger hilt en Home, instrucciones y login

---
+++ app/src/main/java/com/example/equipouno/di/Module.kt
@@ -0,0 +1,39 @@
+package com.example.equipouno.di
+
+import com.example.equipouno.ServicesWeb.ServiceAPI
+import com.example.equipouno.ServicesWeb.RetrofitManagement
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return RetrofitManagement.getRetrofit()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ServiceAPI {
+        return retrofit.create(ServiceAPI::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFirebaseAuth(): FirebaseAuth {
+        return Firebase.auth
+    }
+}
+
+
+
